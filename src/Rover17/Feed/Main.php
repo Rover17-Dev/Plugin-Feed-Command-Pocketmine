8456d42c59c83ba6bf3e0cf474b63247308c2d76
Rover17-Dev <74236531+Rover17-Dev@users.noreply.github.com>
2021-05-25 17:06:55 +0300

    Add files via upload

---
+++ src/Rover17/Feed/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Feed;
+
+use Feed\Command\feed;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getCommandMap()->register("feed", new feed($this));
+    }
+}
