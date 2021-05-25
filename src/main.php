7ff4aa89ee349a64e376a7e4d826d818d88c4233
Rover17-Dev <74236531+Rover17-Dev@users.noreply.github.com>
2021-05-25 16:43:12 +0300

    Update for fix

--- src/main.php
+++ src/main.php
@@ -2,15 +2,14 @@
 
 namespace Feed;
 
-use Feed\Command\Feed;
-use Feed\Command\Heal;
+use Feed\Command\feed;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
+
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getCommandMap()->register("Feed", new Feed($this));
-
+        $this->getServer()->getCommandMap()->register("feed", new feed($this));
     }
 }
